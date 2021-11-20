c6337dc6b1097f5ac93c36bdb5ec2da595947794
zsotroav <zsotroav@gmail.com>
2021-11-20 15:10:30 +0100

    update arduino config ui

--- FOK-GYEM_Ultimate/FormArduinoConfig.Designer.cs
+++ FOK-GYEM_Ultimate/FormArduinoConfig.Designer.cs
@@ -44,7 +44,7 @@ namespace FOK_GYEM_Ultimate
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(10, 11);
+            this.label1.Location = new System.Drawing.Point(12, 12);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(74, 15);
             this.label1.TabIndex = 0;
@@ -53,16 +53,16 @@ namespace FOK_GYEM_Ultimate
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(10, 36);
+            this.label3.Location = new System.Drawing.Point(12, 36);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(79, 15);
+            this.label3.Size = new System.Drawing.Size(106, 15);
             this.label3.TabIndex = 2;
-            this.label3.Text = "Delay in loop:";
+            this.label3.Text = "Delay in loop (ms):";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(10, 62);
+            this.label5.Location = new System.Drawing.Point(12, 62);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(90, 15);
             this.label5.TabIndex = 4;
@@ -83,7 +83,7 @@ namespace FOK_GYEM_Ultimate
             this.ClearCombo.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.ClearCombo.Name = "ClearCombo";
             this.ClearCombo.Size = new System.Drawing.Size(133, 23);
-            this.ClearCombo.TabIndex = 5;
+            this.ClearCombo.TabIndex = 1;
             // 
             // loopClearCombo
             // 
@@ -92,15 +92,15 @@ namespace FOK_GYEM_Ultimate
             "0 No",
             "1 Full 1",
             "2 Full 0",
-            "3 Full 1 and Full 0",
+            "3 Full 1 + Full 0",
             "4 Alternating 101",
             "5 Alternating 010",
-            "6 Alternating 101 and 010 "});
+            "6 Alternating 101 + 010 "});
             this.loopClearCombo.Location = new System.Drawing.Point(122, 59);
             this.loopClearCombo.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.loopClearCombo.Name = "loopClearCombo";
             this.loopClearCombo.Size = new System.Drawing.Size(133, 23);
-            this.loopClearCombo.TabIndex = 6;
+            this.loopClearCombo.TabIndex = 3;
             // 
             // loopCheck
             // 
@@ -111,7 +111,7 @@ namespace FOK_GYEM_Ultimate
             this.loopCheck.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.loopCheck.Name = "loopCheck";
             this.loopCheck.Size = new System.Drawing.Size(93, 19);
-            this.loopCheck.TabIndex = 7;
+            this.loopCheck.TabIndex = 5;
             this.loopCheck.Text = "Loop display";
             this.loopCheck.UseVisualStyleBackColor = true;
             // 
@@ -122,17 +122,32 @@ namespace FOK_GYEM_Ultimate
             this.flipCheck.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.flipCheck.Name = "flipCheck";
             this.flipCheck.Size = new System.Drawing.Size(95, 19);
-            this.flipCheck.TabIndex = 8;
+            this.flipCheck.TabIndex = 4;
             this.flipCheck.Text = "Panel flipped";
             this.flipCheck.UseVisualStyleBackColor = true;
             // 
             // delayNumeric
             // 
+            this.delayNumeric.Increment = new decimal(new int[] {
+            500,
+            0,
+            0,
+            0});
             this.delayNumeric.Location = new System.Drawing.Point(122, 34);
             this.delayNumeric.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.delayNumeric.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
             this.delayNumeric.Name = "delayNumeric";
             this.delayNumeric.Size = new System.Drawing.Size(132, 23);
-            this.delayNumeric.TabIndex = 9;
+            this.delayNumeric.TabIndex = 2;
+            this.delayNumeric.Value = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
             // 
             // button1
             // 
@@ -140,7 +155,7 @@ namespace FOK_GYEM_Ultimate
             this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(243, 22);
-            this.button1.TabIndex = 10;
+            this.button1.TabIndex = 6;
             this.button1.Text = "Export";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
